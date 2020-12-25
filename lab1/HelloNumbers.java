244560c643efcfbc727c77d56883c88d748c49b1
BlueCloud <1301866313@qq.com>
2020-12-25 15:40:21 +0800

    a

--- lab1/HelloNumbers.java
+++
@@ -1,9 +0,0 @@
-public class HelloNumbers {
-    public static void main(String[] args) {
-        int res = 0;
-        for(int i = 1; i < 10; i ++) {
-            res += i;
-        }
-        System.out.println(res);
-    }
-}
