f2a48d194b95e8f4a0693c332ba5517102211e9a
dingqingyun <7638540+dingqingyun@user.noreply.gitee.com>
2021-02-04 20:15:14 +0800

    some change

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,29 +1,32 @@
 public class ArrayDeque<T> {
     private T[] items;
     private int size;
-    private int nextFirst;
-    private int nextLast;
+    private int head;
+    private int tail;
     private int capacity;
+    private static int initialCapacity = 8;
+    /* expand/contract factor */
+    private static int Factor = 2;
+    /* minimum capacity for contraction resizing */
+    private static int mCapacity = 16;
+    /* minimum usage ratio before contraction */
+    private static double mRatio = 0.25;
+
+
     public ArrayDeque() {
-        items = (T[]) new Object[8];
+        items = (T[]) new Object[initialCapacity];
         size = 0;
         capacity = 8;
-        nextFirst = capacity / 2;
-        nextLast = nextFirst + 1;
+        head = capacity - 1;
+        tail = 0;
     }
 
-    public void addFirst(T item) {
-        items[nextFirst] = item;
-        nextFirst = (nextFirst - 1 + capacity) % capacity;
-        size += 1;
-        reLargerSize();
+    private int onePlus(int index) {
+        return (index + 1 + capacity) % capacity;
     }
 
-    public void addLast(T item) {
-        items[nextLast] = item;
-        nextLast = (nextLast + 1 + capacity) % capacity;
-        size += 1;
-        reLargerSize();
+    private int oneMinus(int index) {
+        return (index - 1 + capacity) % capacity;
     }
 
     public boolean isEmpty() {
@@ -38,58 +41,94 @@ public class ArrayDeque<T> {
     }
 
     public void printDeque() {
-        for (int i = nextFirst + 1; items[i] != null; i++) {
-            int index = i % capacity;
-            if (index == nextLast) {
-                break;
-            }
-            System.out.print(items[index] + " ");
+        int currentIndex = onePlus(head);
+        while (currentIndex != tail) {
+            System.out.print(items[currentIndex] + " ");
+            currentIndex = onePlus(currentIndex);
         }
-    }
-
-    public T removeFirst() {
-        if (size == 0) {
-            return null;
-        }
-        nextFirst = (nextFirst + 1 + capacity) % capacity;
-        size -= 1;
-        reShrinkSize();
-        return items[nextFirst + 1];
-    }
-
-    public T removeLast() {
-        if (size == 0) {
-            return null;
-        }
-        nextLast = (nextLast - 1 + capacity) % capacity;
-        size -= 1;
-        reShrinkSize();
-        return items[nextLast - 1];
+        System.out.println();
     }
 
     public T get(int index) {
         if (index < 0 || index >= size) {
             return null;
         }
-        index = (index + nextFirst + 1 + capacity) % capacity;
+        index = (index + head + 1 + capacity) % capacity;
         return items[index];
     }
 
-    private void reLargerSize() {
-        if (size == 8 && nextFirst == nextLast || size >= 16 && capacity / size < 2) {
-            capacity =  2 * capacity;
-            T[] newItems = (T[]) new Object[capacity];
-            System.arraycopy(items, 0, newItems, capacity / 4, capacity / 2);
+    /* if head<-- -->tail, means items is consistently,
+        else if 0<--->tail ----- head<-->items.length, which break into two part.
+     */
+    private void resize(int newCapacity) {
+        T[] newItems = (T[]) new Object[newCapacity];
+        int p = head;
+        int len = items.length;
+        if (head >= tail) {
+            int r = len - p;
+            System.arraycopy(items, p, newItems, 0, r);
+            System.arraycopy(items, 0, newItems, r, p);
             items = newItems;
+            head = 0;
+            tail = len;
+        } else {
+            System.arraycopy(items, 0, newItems, 0, items.length);
+            items = newItems;
+            head = 0;
+            tail = len;
+        }
+
+    }
+
+    private void expand() {
+        if (head == tail) {
+            int newCapacity = capacity * Factor;
+            resize(newCapacity);
         }
     }
 
-    private void reShrinkSize() {
-        if (size / capacity < 0.25 && size >= 16) {
-            capacity =  2 * size;
-            T[] newItems = (T[]) new Object[capacity];
-            System.arraycopy(items, capacity / 4, newItems, 0, capacity / 2);
-            items = newItems;
+    private void contract() {
+        double ratio = size / capacity;
+        if (capacity >= mCapacity && ratio < mRatio) {
+            int newCapacity = capacity / Factor;
+            resize(newCapacity);
         }
     }
+
+
+    public void addFirst(T item) {
+        items[head] = item;
+        head = (head - 1 + capacity) % capacity;
+        size += 1;
+        expand();
+    }
+
+    public void addLast(T item) {
+        items[tail] = item;
+        tail = (tail + 1 + capacity) % capacity;
+        size += 1;
+        contract();
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        head = (head + 1 + capacity) % capacity;
+        size -= 1;
+        contract();
+        return items[head + 1];
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        tail = (tail - 1 + capacity) % capacity;
+        size -= 1;
+        contract();
+        return items[tail - 1];
+    }
+
+
 }
